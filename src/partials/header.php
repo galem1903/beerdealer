782fd176d7bf4a2e59f951f87d2a2b224ee1852a
ThomasPirotte <thomas.pirotte@student.hepl.be>
2024-05-23 23:09:34 +0200

    Header + css + securité

--- src/partials/header.php
+++ src/partials/header.php
@@ -1,11 +1,11 @@
 <header>
     <nav id="menu">
+        <img src="images/logo_beerdealer.png" height="65px" width="65px" alt="Logo de Beer Dealer">
         <ul>
-            <li><img src="images/logo_beerdealer.png" height="100px" width="100px" alt="Logo de Beer Dealer"></li>
             <li><a href="index.php">Accueil</a></li>
             <li><a href="contact.php">Contact</a></li>
             <li><a href="favorite_beers.php">Mes bières favorites</a></li>
         </ul>
+        <a href="src/logout.php" alt="Se connecter">Se déconnecter</a>
     </nav>
-    <h1>BeerDealer</h1>
 </header>
