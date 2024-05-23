573c1832cca13f1e9fee287f83a149304ac6360b
ThomasPirotte <thomas.pirotte@student.hepl.be>
2024-05-23 23:52:38 +0200

    header

--- src/partials/header.php
+++ src/partials/header.php
@@ -3,8 +3,8 @@
         <img src="images/logo_beerdealer.png" height="65px" width="65px" alt="Logo de Beer Dealer">
         <ul>
             <li><a href="index.php">Accueil</a></li>
-            <li><a href="contact.php">Contact</a></li>
             <li><a href="favorite_beers.php">Mes bières favorites</a></li>
+            <li><a href="contact.php">Contact</a></li>
         </ul>
         <a href="src/logout.php" alt="Se connecter">Se déconnecter</a>
     </nav>
