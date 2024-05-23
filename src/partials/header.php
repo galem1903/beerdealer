54a18574989234187f6d90c5e6586545c99e1136
ThomasPirotte <thomas.pirotte@student.hepl.be>
2024-05-23 23:51:16 +0200

    taille image

--- src/partials/header.php
+++ src/partials/header.php
@@ -1,6 +1,6 @@
 <header>
     <nav id="menu">
-        <img src="images/logo_beerdealer.png" height="100px" width="100px" alt="Logo de Beer Dealer">
+        <img src="images/logo_beerdealer.png" height="65px" width="65px" alt="Logo de Beer Dealer">
         <ul>
             <li><a href="index.php">Accueil</a></li>
             <li><a href="contact.php">Contact</a></li>
