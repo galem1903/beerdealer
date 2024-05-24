45218a13cb543fbd7ee51ec3c05174427b801994
ThomasPirotte <thomas.pirotte@student.hepl.be>
2024-05-24 02:12:11 +0200

    favoris

--- src/add_to_favorites.php
+++ src/add_to_favorites.php
@@ -1,22 +1,22 @@
 <?php
 // Inclure le fichier security.php
 require_once 'security/security.php';
-
+ 
 require_once(__DIR__. '/config/mysql.php');
 require_once(__DIR__. '/config/connect.php');
-
+ 
 if (isset($_SESSION['loggedUser'])) {
     $user_id = $_SESSION['loggedUser']['user_id'];
-
+ 
     if (isset($beers[$_POST['add_to_favorite']])) {
         $favorite_beer = $beers[$_POST['add_to_favorite']]['beer'];
-
+ 
         $sql = 'SELECT * FROM beers where name = :name';
         $request = $client->prepare($sql);
         $request->execute([
             'name' => $favorite_beer['name']
         ]);
-
+ 
         // ajoute la bière à la bd si elle n'y est pas déja.
         $result = $request->fetch();
         $beer_id = 0;
@@ -35,14 +35,25 @@ if (isset($_SESSION['loggedUser'])) {
         } else {
             $beer_id = $result['beer_id'];
         }
-
-        $sql = 'INSERT INTO user_beer (user_id, beer_id) VALUES (:user_id, :beer_id)';
+ 
+        $sql = 'SELECT * FROM user_beer where user_id = :user_id AND beer_id = :beer_id';
         $request = $client->prepare($sql);
         $request->execute([
             'user_id' => $user_id,
             'beer_id' => $beer_id
         ]);
+ 
+        $request->fetch();
+        if ($request->rowCount() == 0) {
+            $sql = 'INSERT INTO user_beer (user_id, beer_id, liked) VALUES (:user_id, :beer_id, :liked)';
+            $request = $client->prepare($sql);
+            $request->execute([
+                'user_id' => $user_id,
+                'beer_id' => $beer_id,
+                'liked' => true
+            ]);
+        }
     }
 }
-
+ 
 ?>
